dba7cb283841eeb193e6441c94dc63b63d8897ea
Jakob Kralj <jakob.kralj04@gmail.com>
2022-04-01 11:14:25 +0200

    added library

---
+++ boards/library/Config.h
@@ -0,0 +1,13 @@
+//paste config from server here
+#define STATION_ID "id here"
+#define STATION_KEY "paste key here"
+#define PM10 "id here"
+#define PM5 "id here"
+//how many measurements do you want the station to store
+#define MAXmesSize 4
+//if latitude and longitude is constatnt, you may want to define these
+// #define LATITUDE 46.19474520567524
+// #define LONGITUDE 14.547483959689584
+//if you want, you can also define WIFI and other credentials here
+// #define WIFIssid "test"
+// #define WIFIpass "here"
